26fbb170db1ca5deab1edffc9934ae63f118d719
Halanov Sergiy <dukalis.rak@gmail.com>
2016-03-28 23:43:56 +0300

    =*

---
+++ Assets/glassController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class glassController : MonoBehaviour
+{
+    public Texture2D glassCap;
+
+    void Start()
+    {
+        glassCap = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24,false);
+    }
+    void OnPostRender() {
+        glassCap.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        glassCap.Apply();
+    } 
+  
+}
---
+++ Assets/screen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class screen : MonoBehaviour
+
+{
+
+    public Renderer renderer;
+    Texture2D screenCap;
+    Texture2D border;
+    bool shot = false;
+    private glassController glassController;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject glassControllerObject = GameObject.FindWithTag("MainCamera");
+        glassController = glassControllerObject.GetComponent<glassController>();
+
+        screenCap = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24,false); // 1
+    }
+
+    void OnPostRender()
+    {
+        renderer.material.mainTexture = screenCap;
+      screenCap.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+      screenCap.Apply();
+        
+    }
+
+   
+}
